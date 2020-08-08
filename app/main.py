17b4da0de5be1c868fa25e34dbadd281e6f81a68
Ayush Dobhal <adobhal93@gmail.com>
2020-08-08 14:51:12 -0500

    updated prediction api

--- app/main.py
+++ app/main.py
@@ -289,6 +289,7 @@ def predict():
         filtered_idxs = []
         output = []
         idx = 0
+		transform_tensor_to_image = transforms.ToPILImage()
         for i in range(len(probs)):
             if probs[i] is None:
                 entry = {}
@@ -299,8 +300,11 @@ def predict():
                 output[idx] = entry
                 idx = idx + 1
             else:
-                filtered_images.append(images[i])
-                filtered_idxs.append(i)
+				print(bbox[i][0])
+                face = extract_face(Image.open(io.BytesIO(images[i])), bbox[i][0])
+                img = transform_tensor_to_image(face.cpu())
+                filtered_images.append(img)
+                filtered_idxs.append(ids[i])
         
         class_id, class_name, probs = get_prediction(db_id, filtered_images)
         for i in range(len(class_id)):
